320484f0416f114764463ebe3c0d25a4215da687
Maxence ADNOT <contact@maxenceadnot.me>
2024-11-11 02:12:29 +0100

    Finished tutorial

---
+++ sst.config.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "aws-nextjs-container",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    const vpc = new sst.aws.Vpc("MyVpc", { bastion: true });
+    const redis = new sst.aws.Redis("MyRedis", { vpc });
+    const cluster = new sst.aws.Cluster("MyCluster", { vpc });
+
+    cluster.addService("MyService", {
+      loadBalancer: {
+        ports: [{ listen: "80/http", forward: "3000/http" }],
+      },
+      link: [redis],
+      dev: {
+        command: "npm run dev",
+      },
+    });
+  },
+});
